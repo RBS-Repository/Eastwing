eb36a23a06b316ca23571a7c2a071e8f203487c2
Jeks12 <mrjekshacks12@gmail.com>
2024-11-23 16:46:49 +0800

    Backend connection

--- src/api.js
+++ src/api.js
@@ -111,15 +111,3 @@ export const updateProduct = async (input, product_id) => {
 
   return data;
 };
-
-export const deleteOrder = async (orderId) => {
-  const response = await fetch(`/api/orders/${orderId}`, {
-    method: "DELETE",
-  });
-  
-  if (!response.ok) {
-    throw new Error("Failed to delete order");
-  }
-  
-  return response.json();
-};
